0cc49e255589d12786d0f9e0a68e246575bd35f5
superman76 <cbloodjr@gmail.com>
2016-04-01 16:04:56 -0600

    updated fishDetails with card

--- client/FishComps/FishDetails.js
+++ client/FishComps/FishDetails.js
@@ -2,12 +2,22 @@ var React = require('react');
 
 function FishDetails(props) {
 	console.log(props);
-	return (
-		<div>
-			<h3> Hello, from fish details </h3>
-		</div>
-	)
+		return (
+		      <div className="container">
+		      <br/>
 
+		          <div className="card">
+		            <img className="card-img-top fish-img" src={ props.oneFish.img } alt="some fish"/>
+		              <div className="card-block">
+		              <h4 className="card-title">{ props.oneFish.name }</h4>
+		              <p className="card-text">{ props.oneFish.color }</p>
+		              <p className="card-text">{ props.oneFish.length }</p>
+		              <p className="card-text">Will I eat people? { (props.oneFish.people_eater).toString() }</p>
+		              </div>
+		          </div>
+
+		      </div>
+		)
 };
 
 
